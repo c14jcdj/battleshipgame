880ef3d803cd33bf011645d8830f956216c6a727
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-05-11 14:32:40 -0700

    changes route of form

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -2,7 +2,7 @@ class Computer
   attr_accessor :board, :ships
 
   def initialize
-    @ships = [Ship.new(5), Ship.new(3), Ship.new(4)]
+    @ships = [Ship.new(5, "ac"), Ship.new(4,"battleship"),Ship.new(3,'sub'),Ship.new(3, 'cruiser'),Ship.new(2,'destroyer')]
     @board = Board.new
   end
 
