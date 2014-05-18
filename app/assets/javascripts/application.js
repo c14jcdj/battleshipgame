810c974ceac2a25077e1bdf60f3dc6882014b909
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-05-18 03:26:16 -0700

    adds image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -165,7 +165,7 @@ View.prototype = {
         // $('#player tr:eq(' + row + ') td:lt(11):gt(6)').remove()
         // $('#player tr:nth-child(' + (res[0] + 1) + ') td:nth-child(' + (i + 1) + ')').append('<img src="/assets/battleship.png" position="fixed" colspan="5" height="50px" width="' + tile + 'px">')
         for (i; i < j; i++) {
-            $('#player tr:nth-child(' + (res[0] + 1) + ') td:nth-child(' + (i + 1) + ')').append('<img src="/assets/' + ship + pic + '.png" width="50px">')
+            $('#player tr:nth-child(' + (res[0] + 1) + ') td:nth-child(' + (i + 1) + ')').append('<img src="/assets/' + ship + pic + '.png" height="50px" width="50px">')
             pic = pic + 1;
         }
     },
